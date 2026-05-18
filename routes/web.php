acdadcb0cc533fa308957ed8eeb9082f8e665192
sehyeolchan <nurulalifaputrimuslim@gmail.com>
2026-05-18 10:04:42 +0700

    tambah frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontEndController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [FrontEndController::class, 'index']);
